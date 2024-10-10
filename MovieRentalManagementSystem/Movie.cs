128131402e927f6cd49e00d185d849430e9b0101
vigneswaranbranavan <vigneswaranbranavan@gmail.com>
2024-10-10 21:41:40 +0530

    Created Movie Class

---
+++ MovieRentalManagementSystem/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalManagementSystem
+{
+    internal class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public decimal RentalPrice { get; set; }
+
+        public Movie(string title, string director, decimal rentalPrice)
+        {
+            Title = title;
+            Director = director;
+            RentalPrice = rentalPrice;
+        }
+    }
+}
