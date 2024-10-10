670207b6e1e54e10deca698c275f23bae6fd8874
vigneswaranbranavan <vigneswaranbranavan@gmail.com>
2024-10-10 22:03:53 +0530

    Derive child classes

---
+++ MovieRentalManagementSystem/DigitalMovie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalManagementSystem
+{
+    public class DigitalMovie : Movie
+    {
+        public string Fileforamt { get; set; }
+        public decimal duration { get; set; }
+
+        public DigitalMovie(string title, string director, decimal rentalPrice, string fileformat, decimal duration) : base(title, director, rentalPrice)
+        {
+            this.Fileforamt = fileformat;
+            this.duration = duration;
+        }
+    }
+}
---
+++ MovieRentalManagementSystem/DvdMovie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalManagementSystem
+{
+    public class DvdMovie : Movie
+    {
+        public decimal diskweight { get; set; }
+        public decimal duration { get; set; }
+
+        public DvdMovie(string title, string director, decimal rentalPrice, decimal diskweidht, decimal duration) : base(title, director, rentalPrice)
+        {
+            this.diskweight = diskweidht;
+            this.duration = duration;
+        }
+
+        public override string displaymovieinfo()
+        {
+            return $"{base.displaymovieinfo()},DISKWEIGHT:{diskweight},DURATION:{duration}";
+        }
+    }
+}
